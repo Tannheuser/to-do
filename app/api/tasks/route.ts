d7b5f5138a185dfc0e99425e4bcfd7ec47154654
Aleksey Ryabinin <tannheuser@MPB14.home>
2023-09-10 17:00:31 +0200

    feat: Add tasks API GET request

--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -6,6 +6,17 @@ import { TaskService } from '@/lib/services';
 
 AppDataSource.initialize();
 
+export async function GET(request: Request) {
+  try {
+    const taskService = new TaskService();
+    const task = await taskService.getItems();
+
+    return new NextResponse(JSON.stringify(task));
+  } catch (error) {
+    return new NextResponse(null, { status: 400, statusText: (error as Error).message });
+  }
+}
+
 export async function POST(request: Request) {
   try {
     // TODO: Add input validation
